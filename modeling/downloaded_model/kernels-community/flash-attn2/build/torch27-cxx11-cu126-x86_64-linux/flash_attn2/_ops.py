984c072f8348d59fd1e31df55e46a7019d8adc39
yrsgsda <aaa@gmail.com>
2026-03-23 23:38:26 -0400

    updates with new notebooks

--- modeling/downloaded_model/kernels-community/flash-attn2/build/torch27-cxx11-cu126-x86_64-linux/flash_attn2/_ops.py
+++
@@ -1,9 +0,0 @@
-import torch
-from . import _flash_attn_9e27194
-ops = torch.ops._flash_attn_9e27194
-
-def add_op_namespace_prefix(op_name: str):
-    """
-    Prefix op by namespace.
-    """
-    return f"_flash_attn_9e27194::{op_name}"
